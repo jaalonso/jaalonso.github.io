7013327129c59d54c4d0b6374318249c606d49e4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-05-16 12:38:08 +0200

    Tema 3.

--- materias/PLconProlog/codigosDeTemas/automata.pl
+++ materias/PLconProlog/codigosDeTemas/automata.pl
@@ -1,13 +1,11 @@
-% PD-99: automata.pl
-% Autómata no-determinista
-% Bratko-86 p.99-103.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% automata.pl
+% AutÃ³mata no-determinista.
+% JosÃ© A. Alonso JimÃ©nez <https://jaalonso.github.io>
+% Sevilla, 16-mayo-2022
+% ======================================================================
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Consideremos el autómata representado por
+% ----------------------------------------------------------------------
+% Ejercicio 1. Consideremos el autÃ³mata representado por
 %
 %         a
 %        ____
@@ -24,20 +22,14 @@
 %            e4 <-----------  e3
 %                    b
 %
-% siendo e3 el estado final. Representar el autómata utilizando los siguientes
-% predicados:
-%
+% siendo e3 el estado final. Representar el autÃ³mata utilizando las
+% siguientes relaciones:
 % (1) final(S) que se verifica si X es el estado final.
-%
 % (2) trans(E1,X,E2) que se verifica si se puede pasar del estado E1 al
-% estado E2 usando la letra X.
-%
+%     estado E2 usando la letra X.
 % (3) nulo(E1,E2) que se verifica si se puede pasar del estado E1 al
-% estado E2 mediante un movimiento nulo.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Programa                                                              %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     estado E2 mediante un movimiento nulo.
+% ----------------------------------------------------------------------
 
 final(e3).
 
@@ -50,20 +42,18 @@ trans(e3,b,e4).
 nulo(e2,e4).
 nulo(e3,e1).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Definir la relación
-%      acepta(E,L)
-% que se verifique si el autómata, a partir del estado E, acepta la
+% ----------------------------------------------------------------------
+% Ejercicio 2. Definir la relaciÃ³n
+%    acepta(E,L)
+% que se verifique si el autÃ³mata, a partir del estado E, acepta la
 % lista L. Por ejemplo,
-%      acepta(e1,[a,a,a,b]) => Sí
-%      acepta(e2,[a,a,a,b]) => No
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Programa                                                              %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    ?- acepta(e1,[a,a,a,b]).
+%    true ;
+%    false.
+%
+%    ?- acepta(e2,[a,a,a,b]).
+%    false.
+% ----------------------------------------------------------------------
 
 acepta(E,[]) :-
    final(E).
@@ -74,67 +64,34 @@ acepta(E,L) :-
    nulo(E,E1),
    acepta(E1,L).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Determinar si el autómata acepta la lista [a,a,a,b].
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Sesión                                                                %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% ?- acepta(e1,[a,a,a,b]).
-% Yes
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Determinar los estados a partir de los cuales el autómata acepta la
-% lista [a,b].
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Sesión                                                                %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
+% Ejercicio 3. Determinar los estados a partir de los cuales el autÃ³mata
+% acepta la lista [a,b].
+% ----------------------------------------------------------------------
 
 % ?- acepta(E,[a,b]).
-% E=e1 ;
-% E=e3 ;
-% No
+% E = e1 ;
+% E = e3 ;
+% false.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Determinar las palabras de longitud 3 aceptadas por el autómata a
-% partir del estado e1.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Sesión                                                                %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
+% Ejercicio 4. Determinar las palabras de longitud 3 aceptadas por el
+% autÃ³mata a partir del estado e1.
+% ----------------------------------------------------------------------
 
 % ?- acepta(e1,[X,Y,Z]).
-% X = a
-% Y = a
+% X = Y, Y = a,
 % Z = b ;
-% X = b
-% Y = a
-% Z = b ;
-% No
+% X = Z, Z = b,
+% Y = a ;
+% false.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Definir la relación
-%      acepta_acotada(S,L,N)
-% que se verifique si el autómata, a partir del estado S, acepta la
+% ----------------------------------------------------------------------
+% Ejercicio 5. Definir la relaciÃ³n
+%    acepta_acotada(S,L,N)
+% que se verifique si el autÃ³mata, a partir del estado S, acepta la
 % lista L y la longitud de L es N.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Programa                                                              %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
 
 acepta_acotada(S,[],0) :-
    final(S).
@@ -150,33 +107,22 @@ acepta_acotada(S,L,N) :-
    nulo(S,E1),
    acepta_acotada(E1,L,N).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Buscar las cadenas aceptadas a partir de e1 con longitud 3.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Sesión                                                                %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
+% Ejercicio 6. Buscar las cadenas aceptadas a partir de e1 con longitud
+% 3.
+% ----------------------------------------------------------------------
 
 % ?- acepta_acotada(e1,L,3).
 % L = [a, a, b] ;
 % L = [b, a, b] ;
-% No
+% false.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Definir la relación
-%      acepta_acotada_2(S,L,N)
-% que se verifique si el autómata, a partir del estado S, acepta la
+% ----------------------------------------------------------------------
+% Ejercicio 7. Definir la relaciÃ³n
+%    acepta_acotada_2(S,L,N)
+% que se verifique si el autÃ³mata, a partir del estado S, acepta la
 % lista L y la longitud de L es menor o igual que N.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Programa                                                              %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
 
 acepta_acotada_2(S,[],_N) :-
    final(S).
@@ -192,21 +138,13 @@ acepta_acotada_2(S,L,N) :-
    nulo(S,E1),
    acepta_acotada_2(E1,L,N).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Enunciado                                                             %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Buscar las cadenas aceptadas a partir de e1 con longitud menor o igual a
-% 3. 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Sesión                                                                %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ----------------------------------------------------------------------
+% Ejercicio 8. Buscar las cadenas aceptadas a partir de e1 con longitud
+% menor o igual a 3.
+% ----------------------------------------------------------------------
 
 % ?- acepta_acotada_2(e1,L,3).
 % L = [a, a, b] ;
 % L = [a, b] ;
 % L = [b, a, b] ;
-% No
-
-
+% false.
