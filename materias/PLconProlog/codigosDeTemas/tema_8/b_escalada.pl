35de5da1a3ab01ef6c4e1e73dace9b0e42caf0c4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-05-29 19:49:50 +0200

    Tema 8

--- materias/PLconProlog/codigosDeTemas/tema_8/b_escalada.pl
+++ materias/PLconProlog/codigosDeTemas/tema_8/b_escalada.pl
@@ -25,7 +25,7 @@
 % escalada(?S) se verifica si S es una solución del problema mediante
 % búsqueda en escalada. El procedimiento es
 % + Sea E el estado inicial y H su heurística. La solución S es la
-%   solución obtenida por búsqueda en escalada a partir de 0-[E].
+%   solución obtenida por búsqueda en escalada a partir de H-[E].
 escalada(Sol) :-
    estado_inicial(E),
    heuristica(E,H),
@@ -40,16 +40,16 @@ escalada(Sol) :-
 %     1.2 S es la inversa de [E|R].
 % 2. Si
 %         N=H-[E|C] y
-%     2.1 E1 es un sucesor de E con heurística H1 < H y E no tiene sucesores
-%         con heurística menor que H1
+%     2.1 E1 es un sucesor de E con heurística H1 < H y E no tiene
+%         sucesores con heurística menor que H1
 %    entonces
-%     2.2 S es la solución por por escalada a partir de H1-[E1,E|R].
+%     2.2 S es la solución por escalada a partir de H1-[E1,E|R].
 escalada(_H-[E|R],S) :-
-   estado_final(E),                        % 1.1
-   reverse([E|R],S).                       % 1.2
+   estado_final(E),          % 1.1
+   reverse([E|R],S).         % 1.2
 escalada(H-[E|R],S) :-
-   mejor_sucesor(E,H,E1,H1),
-   escalada(H1-[E1,E|R],S).
+   mejor_sucesor(E,H,E1,H1), % 2.1
+   escalada(H1-[E1,E|R],S).  % 2.2
 
 mejor_sucesor(E,H,E1,H1) :-
    sucesor(E,E1),
